1c68acc94d4d25f3ecbda4f839f1bde4c3edf447
Danil Shpartko <d.shpartko56@gmail.com>
2022-08-20 18:10:34 +0300

    восстановил базовую вёрстку

--- express_money/src/App.vue
+++ express_money/src/App.vue
@@ -1,11 +1,42 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      color="white"
+      flat
+    >
+      <v-container class="py-0 fill-height">
+        <v-avatar
+          class="mr-10"
+          color="grey darken-1"
+          size="32"
+        />
+
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          text
+        >
+          {{ link }}
+        </v-btn>
+
+        <v-spacer />
+
+        <v-responsive max-width="260">
+          <v-text-field
+            dense
+            flat
+            hide-details
+            rounded
+            solo-inverted
+          />
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+    <v-main class="grey lighten-3">
+      <HelloWorld />
+    </v-main>
+  </v-app>
 </template>
 
 <style lang="scss">

711722deba06c78de27f234610d87480c31d027b
Danil Shpartko <d.shpartko56@gmail.com>
2022-08-23 12:26:58 +0300

    Добавил шрифты

--- express_money/src/App.vue
+++ express_money/src/App.vue
@@ -8,7 +8,7 @@
       height="90"
     >
       <v-container 
-        class="py-0 fill-height d-flex align-center"
+        class="py-0 fill-height d-flex align-center justify-space-between"
       >
         <v-img
           class="d-flex align-start"
@@ -16,38 +16,43 @@
           max-width="237"
           :src="require('./assets/logo.png')"
         />
-        
-        <!-- <v-btn
-          class="d-flex justify-end"
-            
-          v-for="link in links"
-          :key="link"
-          text
-        >
-          {{ link }}
-        </v-btn> -->
         <v-btn-toggle
-          v-model="text"
+          class="d-flex flex-row justifu-end font-weight-regular"
           tile
           color="blue accent-3"
           group
         >
-          <v-btn value="main">
+          <v-btn
+            value="main"
+            class="font-weight-regular"
+          >
             Главная
           </v-btn>
 
-          <v-btn value="rules">
+          <v-btn
+            value="rules"
+            class="font-weight-regular"
+          >
             Правила
           </v-btn>
 
-          <v-btn value="FAQ">
+          <v-btn
+            value="FAQ"
+            class="font-weight-regular"
+          >
             FAQ
           </v-btn>
 
-          <v-btn value="reviews">
+          <v-btn
+            value="reviews"
+            class="font-weight-regular"
+          >
             Отзывы
           </v-btn>
-          <v-btn value="contacts">
+          <v-btn
+            value="contacts"
+            class="font-weight-regular"
+          >
             Контакты
           </v-btn>
         </v-btn-toggle>
@@ -55,28 +60,24 @@
 
     
         <v-spacer />
-        <v-btn
+        <v-btn 
+          class="white--text"
           large
-          color="primary"
+          color="#6377F7"
+          max-width="62"
+          max-height="36"
         >
           Войти
         </v-btn>
-        <v-btn
+        <v-btn 
           class="mx-3"
           large
-          color="yellow"
+          color="#F1C613"
+          max-width="105"
+          max-height="36"
         >
           Регистрация
         </v-btn>
-
-        <!-- <v-responsive max-width="260">
-          <v-text-field
-            dense
-            flat
-            hide-details
-            solo-inverted
-          />
-        </v-responsive> -->
       </v-container>
     </v-app-bar>
     <v-main class="grey lighten-3">
@@ -89,7 +90,6 @@
 <script>
 import HelloWorld from './components/HelloWorld';
 
-
 export default {
   name: 'App',
 
@@ -97,16 +97,29 @@ export default {
     HelloWorld
   },
 
-data: () => ({
-      links: [
-        'Главная',
-        'Правила',
-        'FAQ',
-        'Отзывы',
-        'Контакты'
-      ],
-    }),
+  data: () => ({
+    links: [
+      'Главная',
+      'Правила',
+      'FAQ',
+      'Отзывы',
+      'Контакты'
+    ],
+  }),
 };
 </script>
 
 
+<style lang='scss'>
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap');
+
+$body-font-family: 'IMB Plex Sans';
+$title-font: 'IMB Plex Sans';
+
+ .v-application {
+   font-family: $body-font-family, sans-serif !important;
+    .title { // To pin point specific classes of some components
+       font-family: $title-font, sans-serif !important;
+    }
+ }
+</style>
